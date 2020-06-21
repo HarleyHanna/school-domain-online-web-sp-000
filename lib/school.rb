a0de000812df02b82fe40dd61ec9331f498887fd
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-21 11:02:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+  
+  attr_accessor = :school_name
+  
+  ROSTER = {}
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+    @grade = []
+  end
+  
+  def roster
+    @roster
+  end
+    
+  
+  def roster=(name)
+    @roster = name
+  end
+  
+  def add_student
+    @student
+    @grade
+  end
+  
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    if ROSTER.include?(@grade) == true 
+      ROSTER[@grade] << @student
+    else 
+      ROSTER[@grade] = []
+      ROSTER[@grade] << @student
+    end
+    @roster.merge(ROSTER)
+  end
+    
+  
+  
+end 
